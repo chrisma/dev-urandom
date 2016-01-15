0dda55e4a7734f35377420f528f6341203d042fc
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2016-01-15 17:04:25 +0100

    Create tweet function.

--- tweet.py
+++ tweet.py
@@ -4,13 +4,13 @@ from twython import Twython
 from credentials import *
 from os import urandom
 
-if __name__ == '__main__':
-	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
+def random_tweet(account):
 	# https://docs.python.org/2/library/codecs.html#python-specific-encodings
 	status = urandom(140).decode('raw_unicode_escape')
-
-	tweet = twitter.update_status(status=status)
-
+	tweet = account.update_status(status=status)
 	# Gotta like this tweet, after all, we wrote it
-	twitter.create_favorite(id=tweet['id'])
+	account.create_favorite(id=tweet['id'])
+
+if __name__ == '__main__':
+	account = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	random_tweet(account)
